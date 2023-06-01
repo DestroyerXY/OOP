6c75c3599ddaeb6f3127c6b28b93c37946b8b67c
DetroyerXY <ramil1995@bk.ru>
2023-06-01 15:01:49 +0300

    ДомашнееЗадание1

--- lesson1/Program.java
+++ lesson1/Program.java
@@ -23,11 +23,16 @@ public class Program {
         Product bottleOfWater5 = new BottleOfWater("ООО Чистый источник", "Бутылка с водой 5", 115, 3);
         System.out.println(bottleOfWater1.displayInfo());
         Product bottleOfMilk1 = new BottleOfMilk("ООО Домик в деревне", "Цельное 1", 120, 2, 3);
-        Product bottleOfMilk2 = new BottleOfMilk("ООО Домик в деревне", "Цельное 2", 120, 2, 3);
-        Product bottleOfMilk3 = new BottleOfMilk("ООО Домик в деревне", "Цельное 3", 120, 2, 3);
-        Product bottleOfMilk4 = new BottleOfMilk("ООО Домик в деревне", "Цельное 4", 120, 2, 3);
-        Product bottleOfMilk5 = new BottleOfMilk("ООО Домик в деревне", "Цельное 5", 120, 2, 3);
+        Product bottleOfMilk2 = new BottleOfMilk("ООО Домик в деревне", "Цельное 2", 140, 2, 5);
+        Product bottleOfMilk3 = new BottleOfMilk("ООО Домик в деревне", "Цельное 3", 160, 1, 7);
+        Product bottleOfMilk4 = new BottleOfMilk("ООО Домик в деревне", "Цельное 4", 190, 2, 2);
+        Product bottleOfMilk5 = new BottleOfMilk("ООО Домик в деревне", "Цельное 5", 220, 3, 1);
         System.out.println(bottleOfMilk1.displayInfo());
+        Product Chocolate1 = new Chocolate("ООО Баунти", "Шоколад 1", 120, 130, 50);
+        Product Chocolate2 = new Chocolate("ООО Красный Октябрь", "Шоколад 2", 110, 130, 70);
+        Product Chocolate3 = new Chocolate("ООО РотФронт", "Шоколад 3", 100, 300, 20);
+        Product Chocolate4 = new Chocolate("ООО Лейс", "Шоколад 4", 160, 100, 100);
+        System.out.println(Chocolate1.displayInfo());
 
         List<Product> products = new ArrayList<>();
         products.add(bottleOfWater1);
@@ -42,14 +47,19 @@ public class Program {
         products.add(bottleOfMilk4);
         products.add(bottleOfMilk5);
 
+        products.add(Chocolate1);
+        products.add(Chocolate2);
+        products.add(Chocolate3);
+        products.add(Chocolate4);
+
         VendingMachine vendingMachine = new VendingMachine(products);
-        BottleOfWater bottleOfWaterResult = vendingMachine.getBottleOfWater(5);
-        if (bottleOfWaterResult != null){
+        Chocolate ChocolateResult = vendingMachine.getChocolate(100);
+        if (ChocolateResult != null){
             System.out.println("Вы купили: ");
-            System.out.println(bottleOfWaterResult.displayInfo());
+            System.out.println(ChocolateResult.displayInfo());
         }
         else{
-            System.out.println("Такой бутылки нет в автомате");
+            System.out.println("Такого шоколада нет");
         }
     }
 }
